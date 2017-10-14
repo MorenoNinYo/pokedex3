a8424ef965cd113fcf2a27486e2f4fdab9d631fa
lucahernandezacosta <l.hernandezacosta@stud.uni-goettingen.de>
2017-10-14 15:41:18 +0200

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Luca Hernández Acosta on 14.10.17.
+//  Copyright © 2017 Luca Hernández Acosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
